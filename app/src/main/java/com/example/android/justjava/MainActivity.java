0b9dfab4de6ae62016dfd540ecbf7e64c65fa9b9
Michel Bourgeois <promotions332@gmail.com>
2016-09-27 12:30:29 -0700

    pas boolean into order summary method

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         int price= calculatePrice() ;
-        String priceMessage = createOrderSummary(price) ;
+        String priceMessage = createOrderSummary(price,);
         displayMessage(priceMessage);
 
 
@@ -59,11 +59,12 @@ public class MainActivity extends AppCompatActivity {
 
 
     /**
-     *
+     *Create summary of the order
      * @param price of the order
+     * @param addWhippedCream whether or not the client wants whipped cream topping
      * @return text summary
      */
-    private String createOrderSummary(int price){
+    private String createOrderSummary(int price, boolean addWhippedCream){
     String priceMessage = "Name: Kapitan Kunal";
     priceMessage=priceMessage +"\nQuantity:"+ quantity;
     priceMessage=priceMessage +"\nTotal; $"+ price ;
