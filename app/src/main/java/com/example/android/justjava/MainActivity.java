6547d077b8969793e45ca8a2299bff0206c98695
Michel Bourgeois <promotions332@gmail.com>
2016-09-28 10:39:50 -0700

    add editText in xml and get name and put it into a variable testing with log.v

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
 
+import static android.R.attr.value;
 import static com.example.android.justjava.R.string.cream;
 
 /**
@@ -29,53 +31,67 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
-        quantity = quantity + 1 ;
-        displayQuantity (quantity) ;
+        quantity = quantity + 1;
+        displayQuantity(quantity);
     }
+
     /**
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
-        quantity = quantity - 1 ;
-        displayQuantity (quantity) ;
+        quantity = quantity - 1;
+        displayQuantity(quantity);
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        CheckBox whippedCreamCheckBox= (CheckBox)findViewById(R.id.whipped_cream_checkbox);
-        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
-        int price= calculatePrice() ;
-        String priceMessage = createOrderSummary(price,hasWhippedCream);
-        displayMessage(priceMessage);
+        EditText nameField = (EditText)findViewById(R.id.name_text);
+        String name = nameField.getText().toString();
+        Log.v("MainActivity", " Name: " + name);
 
+        //Figure out if client want whipped cream
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+        // Figure out if client want chocolate
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+
+        int price = calculatePrice();
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate);
+        displayMessage(priceMessage);
 
 
     }
 
 
     /**
-     *Create summary of the order
-     * @param price of the order
+     * Create summary of the order
+     *
+     * @param price           of the order
      * @param addWhippedCream whether or not client want whipped cream topping
+     * @param addChocolate    whether or not client want chocolate
      * @return text summary
      */
-    private String createOrderSummary(int price, boolean addWhippedCream){
-    String priceMessage = "Name: Agelita Syphax";
-    priceMessage+="\nAdd whipped cream? "+addWhippedCream;
-    priceMessage+="\nQuantity:"+ quantity;
-    priceMessage+="\nTotal; $"+ price ;
-    priceMessage+="\nThank You!";
-    return priceMessage;
-}
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate) {
+        String priceMessage = "Name: Agelita Syphax";
+        priceMessage += "\nAdd whipped cream? " + addWhippedCream;
+        priceMessage += "\nAdd Chocolate? " + addChocolate;
+        priceMessage += "\nQuantity:" + quantity;
+        priceMessage += "\nTotal; $" + price;
+        priceMessage += "\nThank You!";
+        return priceMessage;
+    }
 
     /**
      * Calculates the price of the order.
-     *@ return total price
+     *
+     * @ return total price
      */
     private int calculatePrice() {
-        return quantity*5 ;
+        return quantity * 5;
 
     }
 
